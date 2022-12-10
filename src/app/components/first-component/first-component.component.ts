d2dfeb7e3cc241075b45c85f369df6ab44068f99
GiselleBarbosa <g88barbosa@gmail.com>
2022-12-10 12:02:17 -0300

    utilizando interpolacao no first component

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstComponentComponent implements OnInit {
 
+  title = 'Titulo no first component';
+
+  nome: string = 'Giselle';
+  idade: number = 33;
+  job: string = "Developer";
+  hobbies: string[] = ['jogar video game', 'estudar', 'assistir filmes e series'];
+
+  videoGame = {
+    plataforma: 'Playstation',
+    versao: 4,
+    marca: 'Sony'
+
+  }
+
   constructor() { }
 
   ngOnInit(): void {
