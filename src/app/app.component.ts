1679e4d3cdf68da9e61053bde19e14e94e639416
GiselleBarbosa <g88barbosa@gmail.com>
2022-12-13 12:42:23 -0300

    testes com input e ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Detalhes } from './components/parent-data/parent-data.component';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +9,33 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
 
-  userName = 'Giselle Barbosa'
-
+  userName = 'Giselle Barbosa';
   nacionalidade = 'Brasil';
 
-  detalhes = {
-    role: 'Developer frontend',
-    email: 'email@mail.com',
-    level: 'estagiaria'
-  }
+  tabelaUsuarios: Detalhes[] = [
+    {
+      nome: 'Giselle Barbosa',
+      email: "giselle@mail.com",
+      senioridade: "Junior",
+      cargo: "Frontend Developer"
+    },
+    {
+      nome: 'Tiago Barbosa',
+      email: "tiago@mail.com",
+      senioridade: "Pleno",
+      cargo: "Backend Developer"
+    },
+    {
+      nome: 'Yago Barbosa',
+      email: "yagogs@mail.com",
+      senioridade: "Senior",
+      cargo: "Fullstack Developer"
+    },
+    {
+      nome: 'Marcos Felipe',
+      email: "mf@mail.com",
+      senioridade: "Estagiario",
+      cargo: "Developer frontend"
+    },
+  ];
 }
--- src/app/components/parent-data/parent-data.component.ts
+++ src/app/components/parent-data/parent-data.component.ts
@@ -1,16 +1,25 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+export interface Detalhes {
+  nome: string;
+  email: string;
+  senioridade: string;
+  cargo: string;
+}
+
 @Component({
   selector: 'app-parent-data',
   templateUrl: './parent-data.component.html',
   styleUrls: ['./parent-data.component.scss']
 })
+
 export class ParentDataComponent implements OnInit {
 
   @Input() name: string = '';
-  @Input() detalhes: any;
 
-  constructor() { }
+  @Input() detalhes: Detalhes[] = [];
+
+  constructor () { }
 
   ngOnInit(): void {
   }
